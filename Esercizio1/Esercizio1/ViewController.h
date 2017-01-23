85845cfa2bc214841ff351337dacd6f532a4036c
Unknown <lcampara96@gmail.com>
2017-01-23 14:57:04 +0100

    upload esercizio 1

---
+++ Esercizio1/Esercizio1/ViewController.h
@@ -0,0 +1,20 @@
+//
+//  ViewController.h
+//  Esercizio1
+//
+//  Created by Luca Campara on 23/01/17.
+//  Copyright © 2017 Luca Campara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property(nonatomic, weak) IBOutlet UILabel *showTextLabel;
+@property(nonatomic, weak) IBOutlet UITextField *insertTextField;
+
+-(IBAction)buttonPressed: (id)sender;
+
+
+@end
+
---
+++ Esercizio1/Esercizio1/ViewController.m
@@ -0,0 +1,42 @@
+//
+//  ViewController.m
+//  Esercizio1
+//
+//  Created by Luca Campara on 23/01/17.
+//  Copyright © 2017 Luca Campara. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+-(void)setLabelText:(NSString *) textToShow {
+    [self.showTextLabel setText:textToShow];
+}
+
+-(IBAction)buttonPressed:(id)sender {
+    NSString *textToShow = [self.insertTextField text];
+    
+    [self setLabelText:textToShow];
+    
+    NSLog(@"valore inserito: %@", textToShow);
+}
+
+
+@end
